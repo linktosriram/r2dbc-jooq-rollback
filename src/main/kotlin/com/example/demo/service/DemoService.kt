01fe522406808774854b1492e250c309d07136d1
Sriram Rajagopalan <6032124+linktosriram@users.noreply.github.com>
2024-01-16 15:39:52 +0200

    initial commit

--- src/main/kotlin/com/example/demo/service/DemoService.kt
+++ src/main/kotlin/com/example/demo/service/DemoService.kt
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.ReactiveTransactionManager
 import org.springframework.transaction.annotation.Propagation
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.reactive.TransactionalOperator
 import org.springframework.transaction.reactive.executeAndAwait
 import org.springframework.transaction.support.DefaultTransactionDefinition
@@ -19,7 +18,6 @@ class DemoService(
 
     private val log = LoggerFactory.getLogger(DemoService::class.java)
 
-    @Transactional
     suspend fun addRecords(): Unit = operator.executeAndAwait {
         log.info("Inserting records")
         repo.insert("a", OffsetDateTime.now())
